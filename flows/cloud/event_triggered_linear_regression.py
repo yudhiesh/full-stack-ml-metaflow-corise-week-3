6f158ff88ec74aaa671a37ee8cfa2f5cfd529f1d
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2023-05-14 08:36:48 +0000

    add week 3

--- flows/cloud/event_triggered_linear_regression.py
+++ flows/cloud/event_triggered_linear_regression.py
@@ -16,21 +16,9 @@ class TaxiFarePrediction(FlowSpec):
             # Try to complete tasks 2 and 3 with this function doing nothing like it currently is.
             # Understand what is happening.
             # Revisit task 1 and think about what might go in this function.
-
-        obviously_bad_data_filters = [
-
-            df.fare_amount > 0,         # fare_amount in US Dollars
-            df.trip_distance <= 100,    # trip_distance in miles
-            df.trip_distance > 0
-
-            # TODO: add some logic to filter out what you decide is bad data!
-            # TIP: Don't spend too much time on this step for this project though, it practice it is a never-ending process.
-
-        ]
-
-        for f in obviously_bad_data_filters:
-            df = df[f]
-            
+        if df.isnull().values.any():
+            print("NaN values found, dropping all rows with any NaN values!")
+            return df.dropna()
         return df
 
     @step
